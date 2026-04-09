81407915615ff8f4ddc4907b7d11f46e3152750c
Moulham Ghanem <molhamghanem9@gmail.com>
2026-04-09 10:46:56 +0300

    feat: implement full file lifecycle
    - Added Open, Save, and Save As functionality. - Implemented dirty flag logic. - Handled FormClosing safety prompt.

--- SharpEdit/Form1.Designer.cs
+++ SharpEdit/Form1.Designer.cs
@@ -28,23 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // rtbText
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1058, 505);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.rtbText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbText.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbText.Location = new System.Drawing.Point(0, 28);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(1058, 505);
+            this.rtbText.TabIndex = 0;
+            this.rtbText.Text = "";
+            this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
             // 
             // menuStrip1
             // 
@@ -53,6 +58,7 @@
             this.newToolStripMenuItem,
             this.openToolStripMenuItem,
             this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
@@ -71,29 +77,44 @@
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
             this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
             this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1058, 533);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.rtbText);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Sharp Edit";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -103,12 +124,15 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtbText;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
     }
 }
 
--- SharpEdit/Form1.cs
+++ SharpEdit/Form1.cs
@@ -1,20 +1,141 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SharpEdit
 {
     public partial class Form1 : Form
     {
+        // Tracks the path of the currently opened file
+        private string _CurrentFilePath = "";
+
+        // Flag to check if the text has been modified since the last save
+        private bool _IsDirty = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Updates the Form title with the file name and an asterisk if there are unsaved changes.
+        /// </summary>
+        private void _UpdateFormTitle()
+        {
+            string fileName = string.IsNullOrEmpty(_CurrentFilePath) ? "Untitled" : Path.GetFileName(_CurrentFilePath);
+            string prefix = _IsDirty ? "*" : "";
+            this.Text = $"{prefix}Sharp Edit - {fileName}";
+        }
+
+        // --- File Menu Operations ---
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _CurrentFilePath = openFileDialog.FileName;
+
+                // Read file content and load it into the RichTextBox
+                using (StreamReader reader = new StreamReader(openFileDialog.OpenFile()))
+                {
+                    rtbText.Text = reader.ReadToEnd();
+                }
+
+                // Reset the dirty flag after loading a new file
+                _IsDirty = false;
+                _UpdateFormTitle();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // If file already exists, save directly. Otherwise, trigger Save As.
+            if (!string.IsNullOrEmpty(_CurrentFilePath))
+            {
+                try
+                {
+                    File.WriteAllText(_CurrentFilePath, rtbText.Text);
+                    _IsDirty = false;
+                    _UpdateFormTitle();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during save: " + ex.Message);
+                }
+            }
+            else
+            {
+                saveAsToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.DefaultExt = "txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _CurrentFilePath = saveFileDialog.FileName;
+                    File.WriteAllText(_CurrentFilePath, rtbText.Text);
+
+                    _IsDirty = false;
+                    _UpdateFormTitle();
+                    MessageBox.Show("File saved successfully!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // This triggers the FormClosing event automatically
+            this.Close();
+        }
+
+        // --- Form Events ---
+
+        private void rtbText_TextChanged(object sender, EventArgs e)
+        {
+            // Mark the file as modified and update title
+            if (!_IsDirty)
+            {
+                _IsDirty = true;
+                _UpdateFormTitle();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Logic to handle unsaved changes before closing
+            if (_IsDirty)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Do you want to save changes to this file?",
+                    "Sharp Edit",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    saveToolStripMenuItem_Click(sender, e);
+                    // If after saving it's still dirty (e.g. Save As was cancelled), stay in the app
+                    if (_IsDirty) e.Cancel = true;
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    // Stop the form from closing
+                    e.Cancel = true;
+                }
+                // If No, the form will close naturally
+            }
         }
     }
 }
